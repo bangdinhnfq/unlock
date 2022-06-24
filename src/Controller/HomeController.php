5d0e013dd2c29bdbb15c35a493fd7acac040199c
Bang Dinh <bang.dinh@nfq.asia>
2022-06-24 14:05:21 +0700

    feat: change hom

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -11,7 +11,7 @@ class HomeController extends AbstractController
      */
     public function getIndexAction(): Response
     {
-        $template = 'home/index.php';
+        $template = 'home/index.php'; // Changed
 
         return $this->response->view($template);
     }
